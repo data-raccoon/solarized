c5ad6ad0db26aa01e6bcbbee4d3ebd62e51c6a8f
Stephan Porz <s.porz@uni-bonn.de>
2013-07-15 12:44:59 +0200

    solarized.py added rebasing; pep8, pylint

--- solarized.py
+++ solarized.py
@@ -1,59 +1,90 @@
+# -*- coding: utf-8 -*-
+
+"""solarized.py
+
+Adusts matplotlibs rcParams to use solarized colors (dark as default).
+You may want to call update_rcparams("light") if you need the light version.
+
+"""
+
+__author__ = "Stephan Porz"
+
 import pylab
 
-def do_solarize(var="dark"):
+COLOR = {"base03":  "#002B36",
+         "base02":  "#073642",
+         "base01":  "#586e75",
+         "base00":  "#657b83",
+         "base0":   "#839496",
+         "base1":   "#93a1a1",
+         "base2":   "#EEE8D5",
+         "base3":   "#FDF6E3",
+         "yellow":  "#B58900",
+         "orange":  "#CB4B16",
+         "red":     "#DC322F",
+         "magenta": "#D33682",
+         "violet":  "#6C71C4",
+         "blue":    "#268BD2",
+         "cyan":    "#2AA198",
+         "green":   "#859900"
+         }
 
-    base03 = "#002B36"
-    base02 = "#073642"
-    base01 = "#586e75"
-    base00 = "#657b83"
-    base0 = "#839496"
-    base1 = "#93a1a1"
-    base2 = "#EEE8D5"
-    base3 = "#FDF6E3"
-    yellow = "#B58900"
-    orange = "#CB4B16"
-    red = "#DC322F"
-    magenta = "#D33682"
-    violet = "#6C71C4"
-    blue = "#268BD2"
-    cyan = "#2AA198"
-    green = "#859900"
+# rebase: using original naming for dark, renamed for light
+DARK = {"03": COLOR["base03"],
+        "02": COLOR["base02"],
+        "01": COLOR["base01"],
+        "00": COLOR["base00"],
+        "0":  COLOR["base0"],
+        "1":  COLOR["base1"],
+        "2":  COLOR["base2"],
+        "3":  COLOR["base3"]
+        }
+LIGHT = {"03": COLOR["base3"],
+         "02": COLOR["base2"],
+         "01": COLOR["base1"],
+         "00": COLOR["base0"],
+         "0":  COLOR["base00"],
+         "1":  COLOR["base01"],
+         "2":  COLOR["base02"],
+         "3":  COLOR["base03"]
+         }
 
-    p_d = {"ytick.color": base0,  # 'k'
-           "xtick.color": base0,  # 'k'
-           "text.color": base0,  # 'k'
-           "savefig.facecolor": base03,  # 'w'
-           "patch.facecolor": blue,  # 'b'
-           "patch.edgecolor": base0,  # 'k'
-           "grid.color": base0,  # 'k'
-           "figure.edgecolor": base03,  # 'w'
-           "figure.facecolor": base02,  # '0.75'
-           "axes.color_cycle": [blue, green, red, cyan, magenta, yellow,
-                                base0],  # ['b', 'g', 'r', 'c', 'm', 'y', 'k']
-           "axes.edgecolor": base0,  # 'k'
-           "axes.facecolor": base03,  # 'w'
-           "axes.lablecolor": base0,  # 'k'
-           }
 
-    p_l = {"ytick.color": base00,  # 'k'
-           "xtick.color": base00,  # 'k'
-           "text.color": base00,  # 'k'
-           "savefig.facecolor": base3,  # 'w'
-           "patch.facecolor": blue,  # 'b'
-           "patch.edgecolor": base00,  # 'k'
-           "grid.color": base00,  # 'k'
-           "figure.edgecolor": base3,  # 'w'
-           "figure.facecolor": base2,  # '0.75'
-           "axes.color_cycle": [blue, green, red, cyan, magenta, yellow,
-                                base00],  # ['b', 'g', 'r', 'c', 'm', 'y', 'k']
-           "axes.edgecolor": base00,  # 'k'
-           "axes.facecolor": base3,  # 'w'
-           "axes.lablecolor": base00,  # 'k'
-          }
+def update_rcparams(mode="dark"):
+    """update_rcparams(mode="dark")
 
-    if var == "dark":
-        pylab.rcParams.update(p_d)
-    elif var == "light":
-        pylab.rcParams.update(p_l)
+    Changes default colors of matplotlib to solarized.
 
-do_solarize()
+    Params
+    ------
+    mode: str
+        Can be "light" or "dark". Defaults to "dark".
+
+    """
+    if mode == "dark":
+        rebase = DARK
+    elif mode == "light":
+        rebase = LIGHT
+
+    params = {"ytick.color": rebase["0"],  # 'k'
+              "xtick.color": rebase["0"],  # 'k'
+              "text.color": rebase["0"],  # 'k'
+              "savefig.facecolor": rebase["03"],  # 'w'
+              "patch.facecolor": COLOR["blue"],  # 'b'
+              "patch.edgecolor": rebase["0"],  # 'k'
+              "grid.color": rebase["0"],  # 'k'
+              "figure.edgecolor": rebase["03"],  # 'w'
+              "figure.facecolor": rebase["02"],  # '0.75'
+              "axes.color_cycle": [COLOR["blue"], COLOR["green"], COLOR["red"],
+                                   COLOR["cyan"], COLOR["magenta"],
+                                   COLOR["yellow"], rebase["0"]],
+              # ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+              "axes.edgecolor": rebase["0"],  # 'k'
+              "axes.facecolor": rebase["03"],  # 'w'
+              "axes.lablecolor": rebase["0"],  # 'k'
+              }
+
+    pylab.rcParams.update(params)
+
+
+update_rcparams()
