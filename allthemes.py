94a383e7599207f3e54ebef732b7b8bd76f4a1cd
Stephan Porz <s.porz@uni-bonn.de>
2013-07-28 17:16:23 +0200

    allthemes.py bugfix, remove help files

--- allthemes.py
+++ allthemes.py
@@ -4,33 +4,33 @@
 import os
 
 # settings
-themes = ['AnnArbor'
-          'Antibes'
-          'Bergen'
-          'Berkeley'
-          'Berlin'
-          'Boadilla'
-          'boxes'
-          'CambridgeUS'
-          'Copenhagen'
-          'Darmstadt'
-          'default'
-          'Dresden'
-          'Frankfurt'
-          'Goettingen'
-          'Hannover'
-          'Ilmenau'
-          'JuanLesPins'
-          'Luebeck'
-          'Madrid'
-          'Malmoe'
-          'Marburg'
-          'Montpellier'
-          'PaloAlto'
-          'Pittsburgh'
-          'Rochester'
-          'Singapore'
-          'Szeged'
+themes = ['AnnArbor',
+          'Antibes',
+          'Bergen',
+          'Berkeley',
+          'Berlin',
+          'Boadilla',
+          'boxes',
+          'CambridgeUS',
+          'Copenhagen',
+          'Darmstadt',
+          'default',
+          'Dresden',
+          'Frankfurt',
+          'Goettingen',
+          'Hannover',
+          'Ilmenau',
+          'JuanLesPins',
+          'Luebeck',
+          'Madrid',
+          'Malmoe',
+          'Marburg',
+          'Montpellier',
+          'PaloAlto',
+          'Pittsburgh',
+          'Rochester',
+          'Singapore',
+          'Szeged',
           'Warsaw'
           ]
 path_tex = "testtalk.tex"
@@ -39,6 +39,12 @@ mytheme = "Frankfurt"
 for theme in themes:
     path_cur = path_tex[:-4] + "_" + theme + path_tex[-4:]
     os.system("cp " + path_tex + " " + path_cur)
-    os.system("sed -i 's/" + mytheme + "/" + theme + "/g " + path_cur)
+    os.system("sed -i 's/" + mytheme + "/" + theme + "/g' " + path_cur)
     os.system("xelatex " + path_cur)
     os.system("xelatex " + path_cur)
+    os.system("rm " + path_cur[:-3] + ".aux")
+    os.system("rm " + path_cur[:-3] + ".log")
+    os.system("rm " + path_cur[:-3] + ".nav")
+    os.system("rm " + path_cur[:-3] + ".out")
+    os.system("rm " + path_cur[:-3] + ".snm")
+    os.system("rm " + path_cur[:-3] + ".toc")
